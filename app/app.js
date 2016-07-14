2a71dc79922d48dd36079c9ab01b328fb5b1a4c5
Koki Takahashi <hakatasiloving@gmail.com>
2016-07-14 19:07:24 +0900

    Test-implement url matcher

--- app/app.js
+++ app/app.js
@@ -7,6 +7,27 @@ if (application.android) {
         if (intent.getAction() === "android.intent.action.SEND" && intent.getType() === "text/plain") {
             var clipData = intent.getClipData();
             var text = clipData.getItemAt(0).getText();
+
+            var matchingURL = text.match(/https?:\/\/[\-A-Za-z0-9+&@#\/%?=~_|!.]+/);
+
+            if (matchingURL !== null) {
+                var url = matchingURL[0];
+
+                /*
+                http.request({
+                    url: 'https://api.hakatashi.com/pixiv2kindle/publish',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    content: 'id=' + encodeURIComponent('1098347'),
+                }).then(function (response) {
+                    console.log(response.content);
+                }, function (e) {
+                    console.log("Error occurred " + e);
+                });
+                */
+            }
         }
     };
 }
