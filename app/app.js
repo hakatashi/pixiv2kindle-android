2249c1c8fff80aecf1298a09481bdbb58d5849b2
Koki Takahashi <hakatasiloving@gmail.com>
2016-07-14 18:48:52 +0900

    Implement Data retrieval from intent

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 var application = require("application");
+var http = require("http");
+
 application.start({ moduleName: "main-page" });
 if (application.android) {
     application.onLaunch = function (intent) {
-        if (intent.getAction() === "android.intent.action.SEND") {
-            console.log("SEND Intent");
+        if (intent.getAction() === "android.intent.action.SEND" && intent.getType() === "text/plain") {
+            var clipData = intent.getClipData();
+            var text = clipData.getItemAt(0).getText();
         }
     };
 }
