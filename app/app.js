8a440a14f69beff6b39036c5d27db459b23a0460
Koki Takahashi <hakatasiloving@gmail.com>
2016-07-14 18:13:10 +0900

    Make app launchable from text intent

--- app/app.js
+++ app/app.js
@@ -1,2 +1,9 @@
 var application = require("application");
 application.start({ moduleName: "main-page" });
+if (application.android) {
+    application.onLaunch = function (intent) {
+        if (intent.getAction() === "android.intent.action.SEND") {
+            console.log("SEND Intent");
+        }
+    };
+}
