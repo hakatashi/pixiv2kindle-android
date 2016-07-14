ce5ab5e17bed4d71414c20a59d2c04265d21abaf
Koki Takahashi <hakatasiloving@gmail.com>
2016-07-14 20:20:41 +0900

    Implement content delivery logic

--- app/app.js
+++ app/app.js
@@ -12,31 +12,42 @@ if (application.android) {
         if (intent.getAction() === "android.intent.action.SEND" && intent.getType() === "text/plain") {
             var clipData = intent.getClipData();
             var text = clipData.getItemAt(0).getText();
-            showToast(text);
 
             var matchingURL = text.match(/https?:\/\/[\-A-Za-z0-9+&@#\/%?=~_|!.]+/);
 
             if (matchingURL !== null) {
                 var url = matchingURL[0];
 
-                console.log(url);
-                showToast(url);
+                var id = null, match = null;
+                if (match = url.match(/id=(\d+)/)) {
+                    id = match[1];
+                } else if (match = url.match(/novel\/(\d+)/)) {
+                    id = match[1];
+                }
 
-                /*
-                http.request({
-                    url: 'https://api.hakatashi.com/pixiv2kindle/publish',
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    content: 'id=' + encodeURIComponent('1098347'),
-                }).then(function (response) {
-                    console.log(response.content);
-                }, function (e) {
-                    console.log("Error occurred " + e);
-                });
-                */
+                if (id === null) {
+                    showToast("Novel ID not found in URL");
+                } else {
+                    showToast("Delivering novel " + id);
+
+                    http.request({
+                        url: 'https://api.hakatashi.com/pixiv2kindle/publish',
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                        },
+                        content: 'id=' + encodeURIComponent(id),
+                    }).then(function (response) {
+                        showToast("Successfully delivered novel " + id);
+                    }, function (e) {
+                        showToast("Error occured: " + e);
+                    });
+                }
+            } else {
+                showToast("No URL found in string");
             }
+        } else {
+            showToast("Please launch app from pixiv share");
         }
     };
 }
