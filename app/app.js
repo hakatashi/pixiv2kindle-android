340f1b0a7ddc424f7fed1bb38fd85a7595622715
Koki Takahashi <hakatasiloving@gmail.com>
2016-07-14 19:54:06 +0900

    Add showToast message

--- app/app.js
+++ app/app.js
@@ -1,17 +1,26 @@
 var application = require("application");
 var http = require("http");
 
+var showToast = function (text) {
+    var Toast = android.widget.Toast;
+    Toast.makeText(application.android.context, text, Toast.LENGTH_LONG).show();
+}
+
 application.start({ moduleName: "main-page" });
 if (application.android) {
     application.onLaunch = function (intent) {
         if (intent.getAction() === "android.intent.action.SEND" && intent.getType() === "text/plain") {
             var clipData = intent.getClipData();
             var text = clipData.getItemAt(0).getText();
+            showToast(text);
 
             var matchingURL = text.match(/https?:\/\/[\-A-Za-z0-9+&@#\/%?=~_|!.]+/);
 
             if (matchingURL !== null) {
                 var url = matchingURL[0];
+
+                console.log(url);
+                showToast(url);
 
                 /*
                 http.request({
